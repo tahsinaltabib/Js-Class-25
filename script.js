8673b97e71bd584dd1d2e2d73c981a10f1d47c2f
tahsinaltabib <nabilarah33@gmail.com>
2025-07-03 10:56:59 +0600

    logic build

--- script.js
+++ script.js
@@ -2,5 +2,17 @@
 // Condition
 // Loop  =>for,while,do while
 
+// for (initialValue; condition;iteration){
+//     //Output
+// }
 
+
+for (let i = 0; i < 100;i++){
+    document.write("I love you so much");  
+}
+
+
+for (let i = 0; i < 100; i++) {
+    console.log("I love you so much", i);
+}
 
