727cf2bf5ad8d95ff75a1490e8890ebdc2464387
tahsinaltabib <nabilarah33@gmail.com>
2025-07-03 10:38:38 +0600

    change

--- script.js
+++ script.js
@@ -2,12 +2,5 @@
 // Condition
 // Loop  =>for,while,do while
 
-let namta=prompt("Enter a number :")
 
-for(let i=1;i<=10;i++){
- if(i!=2){
-     console.log(namta+" X "+i+" = "+namta*i);
- }
-    
-}
 
