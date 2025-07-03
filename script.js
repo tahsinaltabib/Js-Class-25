787b99b1845a6696bc4b9aa021793e19a95fa0eb
tahsinaltabib <nabilarah33@gmail.com>
2025-07-03 10:24:06 +0600

    loop

---
+++ script.js
@@ -0,0 +1,13 @@
+// Varible
+// Condition
+// Loop  =>for,while,do while
+
+let namta=prompt("Enter a number :")
+
+for(let i=1;i<=10;i++){
+ if(i!=2){
+     console.log(namta+" X "+i+" = "+namta*i);
+ }
+    
+}
+
